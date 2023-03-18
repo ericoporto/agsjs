ce183cc64a83e7a0ebbf38a8238b10cb31795260
Érico Porto <ericoporto2008@gmail.com>
2023-03-18 18:31:15 -0300

    Update my_game_files.js

--- tst/my_game_files.js
+++ tst/my_game_files.js
@@ -1,3 +1,1 @@
-var gamefiles = ['acsetup.cfg','my_ags_game.ags'];
-// fill this array with ALL your game files
-// and put them in the same location
+var gamefiles = ['TestTouchStuff.ags', 'acsetup.cfg'];
